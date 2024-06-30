d34f8a536d5fa5d1c000c63581bebbbba0319583
otavio-f <38397566+otavio-f@users.noreply.github.com>
2024-06-30 10:41:28 -0300

    Implementa MainActivity

--- src/main/java/br/otaviof/festafimdeano/MainActivity.java
+++ src/main/java/br/otaviof/festafimdeano/MainActivity.java
@@ -1,6 +1,10 @@
 package br.otaviof.festafimdeano;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +12,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mViewHolder.today = findViewById(R.id.text_today);
+        this.mViewHolder.daysLeft = findViewById(R.id.text_days_left);
+        this.mViewHolder.confirm = findViewById(R.id.button_confirm);
+
+        this.mViewHolder.confirm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_confirm) {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder {
+        TextView today;
+        TextView daysLeft;
+        Button confirm;
     }
 }
