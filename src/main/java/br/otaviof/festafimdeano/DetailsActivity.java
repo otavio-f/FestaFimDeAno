abb65ab99270abdc1c6b39571a369c99dfc9968f
otavio-f <38397566+otavio-f@users.noreply.github.com>
2024-06-29 21:03:14 -0300

    Criar DetailsActivity

---
+++ src/main/java/br/otaviof/festafimdeano/DetailsActivity.java
@@ -0,0 +1,18 @@
+package br.otaviof.festafimdeano;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
